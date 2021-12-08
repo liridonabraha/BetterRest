2ac39c2ae2e7e18c72e452ca4982e91c533a8df7
Dom Bryan <dombguitar@gmail.com>
2021-12-08 12:58:25 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dom Bryan on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
